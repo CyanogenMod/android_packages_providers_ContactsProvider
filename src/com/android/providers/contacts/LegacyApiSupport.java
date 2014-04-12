43ee9659c7962658d2932e302463f1463da6ebf8
kaiyiz <kaiyiz@codeaurora.org>
2014-04-12 14:40:42 +0800

    Contacts: Fix CTS failure in testGroupsTable/testAddToGroup
    Group is inserted into local account if no account is designated,
    but account name and account type are null when query contacts groups.
    
    Query local account if no account designated when query contacts groups.
    
    CRs-Fixed: 645031
    
    Change-Id: I3850ba5cdf222b6273cc1d43ac738067f4e9aaf9

--- src/com/android/providers/contacts/LegacyApiSupport.java
+++ src/com/android/providers/contacts/LegacyApiSupport.java
@@ -1956,8 +1956,9 @@ public class LegacyApiSupport {
             sb.append(" AND " + Groups.ACCOUNT_TYPE + "=");
             DatabaseUtils.appendEscapedSQLString(sb, mAccount.type);
         } else {
-            sb.append(Groups.ACCOUNT_NAME + " IS NULL" +
-                    " AND " + Groups.ACCOUNT_TYPE + " IS NULL");
+            sb.append(Groups.ACCOUNT_NAME + " = '" + AccountWithDataSet.LOCAL.getAccountName() +
+                    "' AND " + Groups.ACCOUNT_TYPE + " = '" +
+                    AccountWithDataSet.LOCAL.getAccountType() + "'");
         }
     }
 
