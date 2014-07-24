e12a6a9651e86b34de93d8003b857819359b9e9f
blong <blong@codeaurora.org>
2014-07-24 17:18:35 +0800

    Add supports for filter SIM contacts
    - Add a flag to filter SIM contacts
    - Use the Phone account as default account, store the contacts
    on the phone for default.
    
    Change-Id: If2219a13b0c915504375d38f9a0bcbf274250ccb
    Signed-off-by: Xiaojing Zhang <zhangx@codeaurora.org>

--- src/com/android/providers/contacts/LegacyApiSupport.java
+++ src/com/android/providers/contacts/LegacyApiSupport.java
@@ -1937,8 +1937,7 @@ public class LegacyApiSupport {
             sb.append(" AND " + RawContacts.ACCOUNT_TYPE + "=");
             DatabaseUtils.appendEscapedSQLString(sb, mAccount.type);
         } else {
-            sb.append(RawContacts.ACCOUNT_NAME + " IS NULL" +
-                    " AND " + RawContacts.ACCOUNT_TYPE + " IS NULL");
+            sb.append("1=1");
         }
     }
 
