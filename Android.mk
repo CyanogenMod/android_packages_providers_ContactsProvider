b3bdb841725c4fd1f3652b762156c7f813a22236
Stephen Bird <sbird@cyngn.com>
2016-04-08 12:33:11 -0700

    Switch to ambientsdk
    Change-Id: Ic4475d88ac7620373ffa9f03a51032e88a55340c

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ LOCAL_JAVA_LIBRARIES := ext telephony-common
 
 LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest_cm.xml
 
-LOCAL_STATIC_JAVA_AAR_LIBRARIES := ambientsdk-internal
+LOCAL_STATIC_JAVA_AAR_LIBRARIES := ambientsdk
 LOCAL_STATIC_JAVA_LIBRARIES += android-common com.android.vcard guava
 
 # The Emma tool analyzes code coverage when running unit tests on the
